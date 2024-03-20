c3babcecab7db46f791b5e2ee16324cc17dd7180
Hoomainani <163409287+Hoomainani@users.noreply.github.com>
2024-03-20 11:21:35 +0330

    Replaced HTP proxies with V2ray proxies

--- ctvbot/proxy.py
+++ ctvbot/proxy.py
@@ -4,7 +4,6 @@ import random
 
 logger = logging.getLogger(__name__)
 
-
 class ProxyGetter:
     def __init__(self, proxy_file_name="proxy_list.txt"):
         self.proxy_list = []
@@ -29,26 +28,18 @@ class ProxyGetter:
 
         for proxy in proxy_list:
             proxy_parts = proxy.split(":")
-            if len(proxy_parts) == 4:
-                username = proxy_parts[2]
-                password = proxy_parts[3]
-                ip_port = ":".join(proxy_parts[0:2])
-
-                if username != "username":
-                    self.proxy_list.append(
-                        {
-                            "server": "http://" + ip_port,
-                            "username": username,
-                            "password": password,
-                        }
-                    )
+            if len(proxy_parts) == 2:
+                ip = proxy_parts[0]
+                port = int(proxy_parts[1])
+                self.proxy_list.append({
+                    "server": f"http://{ip}:{port}"
+                })
 
         random.shuffle(self.proxy_list)
 
     def get_proxy_as_dict(self) -> dict:
         if not self.proxy_list:
             return {}
-
         proxy = self.proxy_list.pop(0)
         self.proxy_list.append(proxy)
         return proxy
